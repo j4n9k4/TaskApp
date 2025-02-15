fac337981ee669daeb737a3c5752c4009956f54d
j4n9k4 <janukananayakkara@gmail.com>
2025-02-15 19:00:09 +0530

    Task 1 and 2 complete -refactored the code to use an enum class for DayPart

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,21 @@
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val dayPart: DayPart,
+    val duration: Int
+)
+enum class DayPart
+{
+    MORNING, AFTERNOON, EVENING
+}
 fun main()
 {
+    val firstEvent = Event(
+        title = "Study Kotlin",
+        description = "Commit to studying Kotlin at least 15 minutes per day.",
+        dayPart = DayPart.EVENING,
+        duration = 15
+     )
 
+    println(firstEvent)
 }
