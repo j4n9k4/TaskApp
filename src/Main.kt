f644eb9e8228db28d8896bc8cb6260cc1e78bf8a
j4n9k4 <janukananayakkara@gmail.com>
2025-02-15 19:18:36 +0530

    Task 3 and 4 complete added the short task functionality

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,27 @@
 data class Event(
     val title: String,
     val description: String? = null,
-    val dayPart: DayPart,
-    val duration: Int
+    val daypart: Daypart,
+    val durationInMinutes: Int
 )
-enum class DayPart
+enum class Daypart
 {
     MORNING, AFTERNOON, EVENING
 }
 fun main()
 {
-    val firstEvent = Event(
-        title = "Study Kotlin",
-        description = "Commit to studying Kotlin at least 15 minutes per day.",
-        dayPart = DayPart.EVENING,
-        duration = 15
-     )
+    val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+    val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+    val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+    val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+    val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+    val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
 
-    println(firstEvent)
+    val events = mutableListOf(event1, event2, event3, event4, event5, event6)
+
+    val shotEvents = events.filter{
+        it.durationInMinutes < 60
+    }
+
+    println("You have ${shotEvents.size} short events")
 }
